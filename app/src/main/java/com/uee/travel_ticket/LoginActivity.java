f968f84d94a44030f61d4909942675a0018ac885
aparna-jay <aparna.jayawardena1999@gmail.com>
2021-10-01 11:41:59 +0530

    Get live location

--- app/src/main/java/com/uee/travel_ticket/LoginActivity.java
+++ app/src/main/java/com/uee/travel_ticket/LoginActivity.java
@@ -2,7 +2,10 @@ package com.uee.travel_ticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
     }
 }
