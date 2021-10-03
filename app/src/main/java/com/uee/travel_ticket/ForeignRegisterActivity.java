f39eb6874a68b8ff2d5b244903c52f13e0f71869
Kasuni_Madhushika <kasunimadhushika77@gmail.com>
2021-10-03 23:53:20 +0530

    create foreign user registration & navigation parts

--- app/src/main/java/com/uee/travel_ticket/ForeignRegisterActivity.java
+++ app/src/main/java/com/uee/travel_ticket/ForeignRegisterActivity.java
@@ -2,25 +2,76 @@ package com.uee.travel_ticket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.uee.travel_ticket.Models.ForeignUserModel;
+import com.uee.travel_ticket.Models.UserModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ForeignRegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    //registration
+    TextView username, passportId, country, address,password, cpassword;
+    List<ForeignUserModel> users;
+    DatabaseReference databaseUsers;
+    Button addUser, login;
+    UserModel user;
 
+//    //for spinner
     Spinner usersSpinner;
     public static String userType;
 
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foreign_register);
+
+        databaseUsers = FirebaseDatabase.getInstance().getReference("ForeignPassanger");
+
+        username = (TextView) findViewById(R.id.fusername);
+        country = (TextView) findViewById(R.id.fcountry);
+        passportId = (TextView) findViewById(R.id.passportId);
+        address = (TextView) findViewById(R.id.faddress);
+        usersSpinner = (Spinner) findViewById(R.id.userSpinner);
+        password = (TextView) findViewById(R.id.password);
+        cpassword = (TextView) findViewById(R.id.cpassword);
+        addUser = (Button) findViewById(R.id.addUser);
+        login = (Button) findViewById(R.id.addUser);
+
+        users = new ArrayList<ForeignUserModel>();
+
+        addUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addUsers();
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+
 
         // spinner element
         usersSpinner = (Spinner) findViewById(R.id.userSpinner);
@@ -28,6 +79,7 @@ public class ForeignRegisterActivity extends AppCompatActivity implements Adapte
         usersSpinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
         // Spinner Drop down elements
         List<String> packages = new ArrayList<String>();
+        packages.add("Package Types");
         packages.add("Single and Returns");
         packages.add("Family Day Passes");
         packages.add("Day Passes");
@@ -48,5 +100,75 @@ public class ForeignRegisterActivity extends AppCompatActivity implements Adapte
     }
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //attaching value event listener
+        databaseUsers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //clearing the previous user list
+                users.clear();
+
+                //iterating through all the nodes
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    //getting Foreign user
+                    ForeignUserModel user = postSnapshot.getValue(ForeignUserModel.class);
+
+                    users.add(user);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addUsers() {
+        //getting the values to save
+        String name1 = username.getText().toString().trim();
+        String passportID1 = passportId.getText().toString().trim();
+        String packageName = usersSpinner.getSelectedItem().toString();
+        String country1 = country.getText().toString().trim();
+        String address1 = address.getText().toString().trim();
+        String password1 = password.getText().toString().trim();
+        //       String genre = spinnerGenre.getSelectedItem().toString();
+
+        //checking if the value is provided
+        if (!TextUtils.isEmpty(name1)) {
+
+            //getting a unique id using push().getKey() method
+            //it will create a unique id and we will use it as the Primary Key for our Artist
+            String id = name1;
+
+            //creating an Artist Object
+            ForeignUserModel user = new ForeignUserModel(id, name1,passportID1, country1 ,address1,password1);
+
+            //Saving the Artist
+            databaseUsers.child(id).setValue(user);
+
+            //setting edittext to blank again
+            username.setText("");
+            passportId.setText("");
+            packageName.compareTo("");
+            country.setText("");
+            address.setText("");
+            password.setText("");
+            cpassword.setText("");
+
+            //displaying a success toast
+            Toast.makeText(this, "Foreign User added", Toast.LENGTH_LONG).show();
+        } else {
+            //if the value is not given displaying a toast
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/uee/travel_ticket/Models/ForeignUserModel.java
@@ -0,0 +1,82 @@
+package com.uee.travel_ticket.Models;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class ForeignUserModel {
+    public String id;
+    public String name;
+    public String passportID;
+//    public String packageName;
+    public String address;
+    public String country;
+    public String password;
+
+    public ForeignUserModel(String id, String name, String passportID, String address, String country, String password) {
+        this.id = id;
+        this.name = name;
+        this.passportID = passportID;
+//        this.packageName = packageName;
+        this.address = address;
+        this.country = country;
+        this.password = password;
+    }
+
+    public ForeignUserModel() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassportID() {
+        return passportID;
+    }
+
+    public void setPassportID(String passportID) {
+        this.passportID = passportID;
+    }
+
+//    public String getPackageName() {
+//        return packageName;
+//    }
+//
+//    public void setPackageName(String packageName) {
+//        this.packageName = packageName;
+//    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
